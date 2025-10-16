7ce5fb83b876e230c6566b7ca3071f0f275a3462
Mir Osayd Ali <mali123@depaul.edu>
2025-10-16 16:48:29 -0500

    Configured more dependencies

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/MyfirstwebappApplication.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -2,9 +2,16 @@ package com.in28minutes.springboot.myfirstwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyfirstwebappApplication {
+public class MyfirstwebappApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyfirstwebappApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstwebappApplication.class, args);
--- src/main/java/com/in28minutes/springboot/myfirstwebapp/controllers/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/controllers/SayHelloController.java
@@ -1,16 +1,42 @@
 package com.in28minutes.springboot.myfirstwebapp.controllers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping
+
+
+@Controller
 public class SayHelloController {
     //"say-hello" => "Hello! What are you learning today?"
     @GetMapping("/say-hello")
     public String sayHello() {
         return "Hello! What are you learning today?";
+    }
+
+    // Trying to send a html page as a response to the browser
+    //@RequestMapping("/say-hello-html")
+    //@ResponseBody
+    //public String sayHelloHtml(){
+    //    StringBuffer sb = new StringBuffer();
+    //    sb.append(“<html>”);
+    //    sb.append(“<head>”);
+    //    sb.append(“<title> My First HTML Page - Changed </title>”);
+    //    sb.append(“</head>”);
+    //    sb.append(“<body>”);
+    //    sb.append(“My First html page with body - Changed”);
+    //    sb.append(“</body>”);
+    //    sb.append(“</html>”);
+
+    //    return sb.toString();
+    //}
+
+    // "say-hello-jsp" => sayHello.jsp
+    @GetMapping("/say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello";
     }
 }
