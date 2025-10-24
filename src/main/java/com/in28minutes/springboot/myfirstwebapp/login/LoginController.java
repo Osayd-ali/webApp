552ab8e416753fd79418182f68c42c2bb3afbb02
Mir Osayd Ali <mali123@depaul.edu>
2025-10-24 12:23:57 -0500

    Added welcome view

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -4,21 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     //http://localhost:8080/login?name=Osayd
     // Model : Whenever you want to send data from the controller to the view, you use Model
-    @RequestMapping("/login")
+    @RequestMapping(value = "login", method = RequestMethod.GET) // or @GetMapping("/login") we can use either one to signify that login runs only for GET requests.
     public String showLoginPage() {
         //model.put("name", name);
         // System.out.println("Request Param is " + name); // NOT RECOMMENDED FOR PRODUCTION CODE
         // logger.debug("Request Param is {}" , name);
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST) // or @PostMapping("/login")we can use either one to signify that login runs only for POST requests.
+    public String showWelcomePage(){
+        return "welcome";
     }
 }
