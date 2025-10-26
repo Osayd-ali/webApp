431e31487bac8f3a636a5ce2c1955229d596db12
Mir Osayd Ali <mali123@depaul.edu>
2025-10-26 14:00:36 -0500

    Returning todo list on view

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,13 +1,26 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class TodoController {
+
+    private TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
     // /list-todos
     @RequestMapping("/list-todos")
-    public String listAllTodos() {
+    public String listAllTodos(ModelMap model) {
+        List<Todo> todos = todoService.findByUsername("admin");
+        model.addAttribute("todos", todos);
         return "listTodos";
     }
 }
